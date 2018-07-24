25486bcc5493c1e34f009fd6b21fbd40cd782fc7
ahuigo <a132811@gmail.com>
2018-07-24 08:26:36 +0800

    add classifiers

--- setup.py
+++ setup.py
@@ -3,23 +3,31 @@ setup for pypi package
 '''
 from setuptools import setup
 
-version = "0.0.1"
+version = "0.0.2"
 
 setup(
     name='xcut',
-    version = version,
+    version=version,
     python_requires='>=3.6.1',
 
     packages=[],
-    #py_modules=['xcut'], # single module
-    install_requires=[ ],
-    scripts = ['xcut'],
-    description = open('README.md').readlines()[1],
+    # py_modules=['xcut'], # single module
+    install_requires=[],
+    scripts=['xcut'],
+    description=open('README.md').readlines()[1].strip(),
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    author = "ahuigo",
-    author_email = "ahui132@qq.com",
-    license = "MIT",
-    url = "http://github.com/ahuigo/xcut",
-)
+    author="ahuigo",
+    author_email="ahui132@qq.com",
+    license="MIT",
+    url="http://github.com/ahuigo/xcut",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
 
+)
