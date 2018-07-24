ae743c9c1a65d2f75ceb1cad7465c34d44cd0927
ahuigo <a132811@gmail.com>
2018-07-24 20:33:41 +0800

    py test

--- test_cli.py
+++ test_cli.py
@@ -2,7 +2,7 @@ from subprocess import getoutput as sh
 
 class TestClass:
     def test_main(self):
-        cmd = "echo 'Lucy,Artist,16' | python xcut -f 1,3 -od $'\t' -t index "
+        cmd = "echo 'Lucy,Artist,16' | python xcut -f 1,3 -od '\t' -t index "
         out = sh(cmd)
         assert "1	3" in out
         assert "Lucy	16" in out
